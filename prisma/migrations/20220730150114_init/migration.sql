93517df7820287ceab994ce509e72ec26f1f7ece
Alexander Dmitriev <dmitriealeksan@yandex.ru>
2022-08-03 18:42:34 +0300

    Server started

---
+++ prisma/migrations/20220730150114_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" SERIAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "carts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,76 @@
 // es modules
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
+require('express-async-errors');
+import bcryptjs from "bcryptjs";
+import { z, ZodError } from "zod";
+import { db } from "./db";
+import { Prisma } from "@prisma/client";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express()
+app.use(express.json())
 
-app.get('/api/hello', (req, res) => {
-    res.send('Hello')
+// const validator: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+//     const user = req.body
+//     if (typeof user.name === 'string'
+//         && typeof user.email === 'string'
+//         && typeof user.password === 'string'
+//         && Object.keys(user).length === 3) return next()
+//     res.send({ error: "Wrong user" })
+// }
+
+app.post('/api/users/signup', async (req, res, next) => {
+    const User = z.object({
+        name: z.string().optional(),
+        email: z.string().email(),
+        password: z.string(),
+    });
+    const user = await User.parseAsync(req.body)
+    const userFromDb = await db.user.create({
+        data: {
+            name: user.name || null,
+            email: user.email,
+            passwordHash: await bcryptjs.hash(user.password, 10),
+        },
+        select: {
+            email: true,
+            name: true,
+            id: true,
+        }
+    })
+    res.send(userFromDb)
 })
+
+app.post('/api/users/login', async (req, res) => {
+    const User = z.object({
+        email: z.string().email(),
+        password: z.string(),
+    });
+    const user = await User.parseAsync(req.body)
+    const userFromDb = await db.user.findUnique({
+        where: {
+            email: user.email
+        }
+    })
+    if (userFromDb) {
+        if (await bcryptjs.compare(user.password, userFromDb.passwordHash)) {
+            // Cookies TODO
+            return res.send('Ok')
+        }
+    }
+    throw new createHttpError.Unauthorized('Wrong user\'s credentials')
+})
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    if (err instanceof ZodError) res.status(400)
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') res.status(409)
+    }
+    if (err instanceof HttpError) res.status(err.status)
+    console.error(err)
+    res.send(err)
+}
+
+app.use(errorHandler)
 
 app.listen(4000)
